316a866bce714b9232e2df59babec49298dee6a3
mroobert <robertmihairo94@gmail.com>
2022-01-23 22:40:11 +0200

    fix deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t mroobert/multi-client:latest -t mroobert/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t mroobert/multi-server:latest -t mroobert/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t mroobert/multi-worker:latest -t mroobert/worker:$SHA -f ./worker/Dockerfile ./worker 
+docker build -t mroobert/multi-worker:latest -t mroobert/multi-worker:$SHA -f ./worker/Dockerfile ./worker 
 
 docker push mroobert/multi-client:latest
 docker push mroobert/multi-server:latest
@@ -10,7 +10,7 @@ docker push mroobert/multi-client:$SHA
 docker push mroobert/multi-server:$SHA
 docker push mroobert/multi-worker:$SHA
 
-kubectl apply -f  k8s
+kubectl apply -f k8s
 kubectl set deployments/client-deployment client=mroobert/multi-client:$SHA
 kubectl set deployments/server-deployment server=mroobert/multi-server:$SHA
 kubectl set deployments/worker-deployment worker=mroobert/multi-worker:$SHA 
